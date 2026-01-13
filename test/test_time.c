76099cf8e164f5c42276066de5dbf33154265b31
kzryzstof <kzryzstof@proton.me>
2026-01-12 19:53:40 -0500

    Remove outdated TODO comment for callback support in xbox_achievements_text_source.c

--- test/test_time.c
+++ test/test_time.c
@@ -7,8 +7,7 @@
 void setUp(void) {}
 void tearDown(void) {}
 
-static void test_time_iso8601_utc_z_to_unix_with_fraction(void)
-{
+static void test_time_iso8601_utc_z_to_unix_with_fraction(void) {
     //  Arrange.
     const int64_t expected_unix_seconds = 1769391470LL;
     const int32_t expected_fraction_ns  = 379172700;
@@ -17,7 +16,8 @@ static void test_time_iso8601_utc_z_to_unix_with_fraction(void)
     int64_t actual_unix_seconds = 0;
     int32_t actual_fraction_ns  = 0;
 
-    bool succeeded = time_iso8601_utc_to_unix("2026-01-26T01:37:50.3791727Z", &actual_unix_seconds, &actual_fraction_ns);
+    bool succeeded =
+        time_iso8601_utc_to_unix("2026-01-26T01:37:50.3791727Z", &actual_unix_seconds, &actual_fraction_ns);
 
     //  Assert.
     TEST_ASSERT_TRUE(succeeded);
@@ -27,8 +27,7 @@ static void test_time_iso8601_utc_z_to_unix_with_fraction(void)
     TEST_ASSERT_EQUAL_INT32(expected_fraction_ns, actual_fraction_ns);
 }
 
-static void test_time_iso8601_utc_z_to_unix_without_fraction(void)
-{
+static void test_time_iso8601_utc_z_to_unix_without_fraction(void) {
     //  Arrange.
     const int64_t expected_unix_seconds = 1769391470LL;
     const int32_t expected_fraction_ns  = 0;
@@ -46,11 +45,9 @@ static void test_time_iso8601_utc_z_to_unix_without_fraction(void)
     TEST_ASSERT_EQUAL_INT32(expected_fraction_ns, actual_fraction_ns);
 }
 
-int main(void)
-{
+int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_time_iso8601_utc_z_to_unix_with_fraction);
     RUN_TEST(test_time_iso8601_utc_z_to_unix_without_fraction);
     return UNITY_END();
 }
-
