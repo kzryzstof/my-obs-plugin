5cf16ee1655415a4cf14b776a2aede17fe709508
kzryzstof <kzryzstof@proton.me>
2026-01-18 08:10:26 -0500

    Add test_parsers executable and initial test setup

---
+++ test/test_parsers.c
@@ -0,0 +1,13 @@
+#include "unity.h"
+
+#include "time/time.h"
+
+#include <string.h>
+
+void setUp(void) {}
+void tearDown(void) {}
+
+int main(void) {
+    UNITY_BEGIN();
+    return UNITY_END();
+}
